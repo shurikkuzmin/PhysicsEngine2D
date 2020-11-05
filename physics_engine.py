c055c325c933055e0e5ef469a99ff3e25a8abf8a
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2020-11-04 22:43:43 -0500

    Adding Box class.

--- physics_engine.py
+++ physics_engine.py
@@ -6,6 +6,7 @@ Created on
 
 import pygame, sys
 from pygame.locals import *
+import numpy
 
 pygame.init()
 
@@ -14,8 +15,26 @@ WINDOWWIDTH=800
 
 SURFACE = pygame.display.set_mode((WINDOWWIDTH,WINDOWHEIGHT))
 
+
+class Box:
+    def __init__(self, centerX, centerY, width, height):
+        self.rect = pygame.Rect(centerX - width/2, centerY-height/2, width, height)
+
+    def update(self):
+        pygame.draw.rect(SURFACE, (255,0,0), self.rect)
+
+box1 = Box(100,100,20,20)
+
 while True:
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        #if event.type = KEYDOWN:
+        #    if event.key = KEY_Q:
+        #        pygame.quit()
+        #        sys.exit()
+        
+    box1.update()
+    pygame.display.update()
