f2466424aa0a4cdef2fb412d1453be3c2a8c06f3
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2020-11-19 23:56:24 -0500

    Fixed the problem with approximation.

--- physics_engine.py
+++ physics_engine.py
@@ -7,11 +7,18 @@ Created on
 import pygame, sys
 from pygame.locals import *
 import numpy
+import random
 
 pygame.init()
 
 WINDOWHEIGHT=600
 WINDOWWIDTH=800
+SPEED = 60
+GRAVITY = 50.0
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+clock = pygame.time.Clock()
 
 SURFACE = pygame.display.set_mode((WINDOWWIDTH,WINDOWHEIGHT))
 
@@ -19,22 +26,45 @@ SURFACE = pygame.display.set_mode((WINDOWWIDTH,WINDOWHEIGHT))
 class Box:
     def __init__(self, centerX, centerY, width, height):
         self.rect = pygame.Rect(centerX - width/2, centerY-height/2, width, height)
+        self.velX = float(random.randint(-20, 20))
+        self.velY = float(random.randint(-20, 20))
+        self.centerX = self.rect.centerx
+        self.centerY = self.rect.centery
+        self.isActive = True
 
     def update(self):
-        pygame.draw.rect(SURFACE, (255,0,0), self.rect)
+        if self.isActive:
+            self.velY = self.velY + GRAVITY * 1.0 / SPEED
+            self.velX = self.velX
+            self.centerX = self.centerX + self.velX / SPEED
+            self.centerY = self.centerY + self.velY / SPEED
+        
+            self.rect.centery = int(self.centerY)
+            self.rect.centerx = int(self.centerX)
+            if self.rect.bottom >= WINDOWHEIGHT:
+                self.rect.bottom = WINDOWHEIGHT - 1
+                self.isActive = False
+        pygame.draw.rect(SURFACE, WHITE, self.rect)
+        
 
-box1 = Box(100,100,20,20)
-
+boxes = []
 while True:
 
+    SURFACE.fill(BLACK)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+            boxes.append(Box(pos[0], pos[1], 30, 30))
+    
+    for box in boxes:
+        box.update()
+            
         #if event.type = KEYDOWN:
         #    if event.key = KEY_Q:
         #        pygame.quit()
         #        sys.exit()
-        
-    box1.update()
+    clock.tick(SPEED)       
     pygame.display.update()
