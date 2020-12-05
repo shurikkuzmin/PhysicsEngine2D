c60559abb03ecdc73e9bd3a75956c83670c7dcaa
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2020-12-05 09:45:47 -0500

    Adding circles.

--- physics_engine.py
+++ physics_engine.py
@@ -33,21 +33,55 @@ class Box:
         self.isActive = True
 
     def update(self):
-        if self.isActive:
-            self.velY = self.velY + GRAVITY * 1.0 / SPEED
-            self.velX = self.velX
-            self.centerX = self.centerX + self.velX / SPEED
-            self.centerY = self.centerY + self.velY / SPEED
+        if not self.isActive:
+            pygame.draw.rect(SURFACE, WHITE, self.rect)
+            return
         
-            self.rect.centery = int(self.centerY)
-            self.rect.centerx = int(self.centerX)
-            if self.rect.bottom >= WINDOWHEIGHT:
-                self.rect.bottom = WINDOWHEIGHT - 1
-                self.isActive = False
+        self.velY = self.velY + GRAVITY * 1.0 / SPEED
+        self.velX = self.velX
+        self.centerX = self.centerX + self.velX / SPEED
+        self.centerY = self.centerY + self.velY / SPEED
+        
+        self.rect.centery = int(self.centerY)
+        self.rect.centerx = int(self.centerX)
+        if self.rect.bottom >= WINDOWHEIGHT:
+            self.rect.bottom = WINDOWHEIGHT - 1
+            self.centerX = self.rect.centerx
+            self.centerY = self.rect.centery
+            self.isActive = False
         pygame.draw.rect(SURFACE, WHITE, self.rect)
-        
 
-boxes = []
+class Circle:
+    def __init__(self, centerX, centerY, radius):
+        self.rect = pygame.Rect(centerX - radius, centerY - radius, 2*radius, 2*radius)
+        self.velX = float(random.randint(-20, 20))
+        self.velY = float(random.randint(-20, 20))
+        self.centerX = self.rect.centerx
+        self.centerY = self.rect.centery
+        self.radius = radius
+        self.isActive = True
+
+    def update(self):
+        if not self.isActive:
+            pygame.draw.circle(SURFACE, WHITE, (self.rect.centerx,self.rect.centery), self.radius)
+            return
+        
+        self.velY = self.velY + GRAVITY * 1.0 / SPEED
+        self.velX = self.velX
+        self.centerX = self.centerX + self.velX / SPEED
+        self.centerY = self.centerY + self.velY / SPEED
+        
+        self.rect.centery = int(self.centerY)
+        self.rect.centerx = int(self.centerX)
+        if self.rect.bottom >= WINDOWHEIGHT:
+            self.rect.bottom = WINDOWHEIGHT - 1
+            self.centerX = self.rect.centerx
+            self.centerY = self.rect.centery
+            self.isActive = False
+        pygame.draw.circle(SURFACE, WHITE, (self.rect.centerx,self.rect.centery), self.radius)       
+
+#boxes = []
+circles = []
 while True:
 
     SURFACE.fill(BLACK)
@@ -57,10 +91,13 @@ while True:
             sys.exit()
         if event.type == MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
-            boxes.append(Box(pos[0], pos[1], 30, 30))
+            #boxes.append(Box(pos[0], pos[1], 30, 30))
+            circles.append(Circle(pos[0], pos[1], 30))
     
-    for box in boxes:
-        box.update()
+    #for box in boxes:
+    #    box.update()
+    for circle in circles:
+        circle.update()
             
         #if event.type = KEYDOWN:
         #    if event.key = KEY_Q:
